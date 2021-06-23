dccc67119692c068d8fa19ddb197cf9943f31d77
zaenury <adhiimzaenury@gmail.com>
2021-06-23 17:18:32 +0800

    Container Widget

---
+++ latihan_container/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Laatihan Container"),
+        ),
+        body: Container(
+          color: Colors.red,
+          margin: EdgeInsets.fromLTRB(10, 15, 20, 25),
+          padding: EdgeInsets.only(top: 20, bottom: 20),
+          child: Container(
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [Colors.amber, Colors.blue])),
+          ),
+        ),
+      ),
+    );
+  }
+}
