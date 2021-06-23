5e1c39281761fb6fc83da54a16dcc9a4a66014cb
zaenury <adhiimzaenury@gmail.com>
2021-06-23 16:09:05 +0800

    text widget

---
+++ text_widget/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Aplikasi Hello World"),
+        ),
+        body: Center(
+          child: Container(
+              color: Colors.lightBlue,
+              width: 150,
+              height: 100,
+              child: Text(
+                "Saya sedang melatih kemampuan flutter saya",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 20),
+              )),
+        ),
+      ),
+    );
+  }
+}
